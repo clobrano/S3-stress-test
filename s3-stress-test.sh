834e9d5574b8e1b5f95a73ea25d8ca78c197b6a8
Carlo Lobrano <Carlo.Lobrano@telit.com>
2016-05-10 15:30:57 +0200

    Optimized test cases

--- s3-stress-test.sh
+++ s3-stress-test.sh
@@ -5,34 +5,56 @@
 # * Check modem is registered through ModemManager
 # * Check modem is connected through ppp
 
-VER=0.3.1
+VER=0.3.2
 
 exec 1> >(logger -s -t $(basename $0)) 2>&1
 
 # Device configuration
 VID=413c
 DEV_ID="1-8.1"
-PERST_PATH=/sys/bus/usb/devices/$DEV_ID/power/persist
 
 # Test configuration
-DISABLE_PERST=1
 IFACE=ppp0
 NTESTS=200
 RETRIES=10
 S3_DURATION=10
-SHUTDOWN_MM=0
-WAIT_DEVICE_TIME=1
 WAIT_CONNECTION_TIME=10
-CHECK_CONNECTION=1
-CHECK_REGISTRATION=1
+WAIT_DEVICE_TIME=1
 
-while getopts "p:n:m:d:i:" opt; do
+while getopts "cd:mn:p:s" opt; do
     case $opt in
-    d)
-        PID=$OPTARG
-        ;;
+        c)
+            CHECK_CONNECTION=1
+            ;;
+        d)
+            DEV_ID=$OPTARG
+            ;;
+        m)
+            SHUTDOWN_MM=1
+            ;;
+        n)
+            NTESTS=$OPTARG
+            ;;
+        p)
+            DISABLE_PERST=1
+            ;;
+        s)
+            CHECK_SERIAL=1
+            ;;
+    esac
+done
+
+echo $CHECK_CONNECTION, $DEV_ID, $SHUTDOWN_MM, $DISABLE_PERST, $CHECK_SERIAL
+
+exit 1
+
+while getopts "c:d:i:m:n:p:r:" opt; do
+    case $opt in
     c)
         CHECK_CONNECTION=$OPTARG
+        ;;
+    d)
+        PID=$OPTARG
         ;;
     i)
         DEV_ID=$OPTARG
@@ -41,107 +63,141 @@ while getopts "p:n:m:d:i:" opt; do
         SHUTDOWN_MM=$OPTARG
         ;;
     n)
-        NTESTS=200
+        NTESTS=$OPTARG
         ;;
     p)
         DISABLE_PERST=$OPTARG
         ;;
     r)
-        CHECK_REGISTRATION=$OPTARG
+        CHECK_COMMUNICATION=$OPTARG
         ;;
 esac
 done
 
-function is_device_present () {
-    [ 1 -eq $(lsusb | grep $VID:$PID | wc -l) ] && echo 1 || echo 0
+PERST_PATH=/sys/bus/usb/devices/$DEV_ID/power/persist
+
+function log () {
+    echo "[+] " $@
 }
 
-function is_registered () {
-    # Get modem id
-    echo "<<< is registered?"
-    mmid=$(mmcli -L | grep -Po "/\d+" | cut -d / -f 2)
-    echo "<<< $mmid"
+function err () {
+    log "[ERROR]" $@
+}
 
-    [ 0 -eq $(mmcli -m $mmid --command=+CSQ 2>&1 | grep error | wc -l) ] && return 1
+function log_start() {
+    echo "<<< Test #$i/$NTESTS: suspend $PID for $S3_DURATION sec. KILL_MM:$SHUTDOWN_MM. DIS_PERST:$DISABLE_PERST. DEV_ID:$DEV_ID."
+}
+
+function check_device_presence () {
+    if [ 1 -eq $(lsusb | grep $VID:$PID | wc -l) ]; then
+       log "Device $VID:$PID found"
+       lsusb
+       return 1
+    fi
     return 0
 }
 
-function is_connected () {
+function check_device_communication () {
+    if [ -z $CHECK_SERIAL ]; then
+        log "Skipping Test communication with serial device"
+        return 1
+    fi
+
+    log "Checking communication with device"
+    cmd="+CSQ"
+    mmid=$(mmcli -L | grep -Po "/\d+" | cut -d / -f 2)
+    mmcli -m $mmid --command="$cmd"
+
+    if [ 0 -eq $(mmcli -m $mmid --command="$cmd"? 2>&1 | grep error | wc -l) ]; then
+        log "Communication test PASSED"
+        return 1
+    fi
+
+    err "Communication test FAILED"
+    return 0
+}
+
+function check_connection () {
+    if [ -z $CHECK_CONNECTION ]; then
+        log "Skipping connection check"
+        return 1
+    fi
+
+    log "Checking connection"
     if [ 1 -eq $(ifconfig $IFACE | wc -l) ]; then
         ping 8.8.8.8 -I $IFACE -c 3
         if [ 0 -eq $? ]; then
-            echo 1
-            return
+            log "Connection Test PASSED"
+            return 1
         fi
     fi
-    echo 0
+    err "Connection test FAILED"
+    return 0
 }
 
+function check_persistence () {
+    log "Current USB persistence value $(cat $PERST_PATH)."
+    if  [ ! -z $DISABLE_PERST ]; then
+        echo 0 > $PERST_PATH
+        log "New USB persistence value $(cat $PERST_PATH)."
+        sleep 1
+    fi
+}
+
+
+# MAIN
 if [ 1 -eq $SHUTDOWN_MM ]; then
-    echo "<<< Shutting down MM"
+    log "Shutting down MM"
     systemctl stop ModemManager
 else
-    echo "<<< Keep MM up and running"
+    log "Keep MM up and running"
 fi
 
 for i in $(seq $NTESTS); do
-    echo "<<< Test #$i/$NTESTS: suspend $PID for $S3_DURATION sec. MM:$SHUTDOWN_MM. DIS_PERST:$DISABLE_PERST. DEV_ID:$DEV_ID."
+    log_start
 
-    echo "<<< Current persistent value $(cat $PERST_PATH)."
-    if  [ 1 -eq $DISABLE_PERST ]; then
-        echo 0 > $PERST_PATH
-        echo "<<< New persistent value $(cat $PERST_PATH)."
-        sleep 1
-    fi
+    check_persistence
 
     rtcwake -m mem -s $S3_DURATION
- 
+USB persistence value
     echo "<<< Test S3 #$i/$NTESTS: wake up"
+
     ret=0
     for j in $(seq $RETRIES); do
-        if [ 0 -ne $PID ]; then
-            [ 0 -eq $ret ] && [ 0 -eq $(is_device_present) ] && sleep 1 && continue
-            lsusb | grep $VID:$PID
-            ret=1
-        fi
 
+        ret=-1
+        check_device_presence
+        [ 0 -eq $? ] && continue
+
+        ret=-2
+        check_device_communication
+        [ 0 eq $? ] && continue
+
+        ret=-3
         sleep $WAIT_CONNECTION_TIME
+        check_connection
+        [ 0 eq $? ] && continue
 
-        if [ 1 -eq $CHECK_REGISTRATION ]; then
-            is_registered
-            [ 1 -eq $? ] && continue
-            ret=3
-            break
-        fi
-
-        if [ 1 -eq $CHECK_CONNECTION ]; then
-            [ 0 -eq $(ifconfig $IFACE | wc -l) ] && continue
-            ping -I $IFACE -c 3 8.8.8.8
-            [ 0 -ne $? ] && continue
-            echo "<<< Device reconnected!"
-            ret=2
-        fi
-        
+        # All tests passed
+        ret=0
         break
     done
 
     case $ret in
-        0)
+        -1)
            echo "<<< Could not find device $VID:$PID"
            lsusb
            ;;
-        1)
-            echo "<<< Could not connect"
+        -2)
+            echo "<<< Could not communicate with serial device"
             ;;
-        2)
-            echo "<<< Test #$i passed!"
-            ;;
-        3)
-            echo "<<< Could not register"
+        -3)
+            echo "<<< Modem is not connected"
             ;;
     esac
 
-    [ 2 -ne $ret ] && echo "<<< Test #$i NOT passed" && break;
+    [ $ret -lt 0 ] && echo "<<< Test #$i NOT passed" && break;
+    echo "<<< Test #$i passed!"
 done
 
 
